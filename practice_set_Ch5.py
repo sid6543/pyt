a80de05c0fae24659490a6a11a70aab8852a15f7
Siddharth Jain <*siddharthjainujn@gmail.com*>
2022-09-29 19:22:12 -0500

    Python Training

---
+++ practice_set_Ch5.py
@@ -0,0 +1,54 @@
+# Question- 1
+'''
+hindi = {
+    "dayalu" : "kind",
+    "rajdhani" : "capital",
+    "khush" : "happy",
+    "dukhi" : "sad",
+
+}
+print("Options are: ", hindi.keys())
+a = input("Enter the word: \n")
+print("The meaning of your word is: ",hindi[a])
+
+
+# Question - 2
+
+num1 = int(input("Enter 1st number \n"))
+num2 = int(input("Enter 2nd number \n"))
+num3 = int(input("Enter 3rd number \n"))
+num4 = int(input("Enter 4th number \n"))
+num5 = int(input("Enter 5th number \n"))
+num6 = int(input("Enter 6th number \n"))
+num7 = int(input("Enter 7th number \n"))
+num8 = int(input("Enter 8th number \n"))
+
+a = {num1, num2, num3, num4, num5, num6, num7, num8}
+print(a)
+
+
+# Question - 3
+s = {18,'18',18.5}
+
+print(len(s))
+print(s)
+'''
+
+# Question -5
+
+language = {}
+
+l1 = input("Enter fav language Sam: \n")
+l2 = input("Enter fav language Bill: \n")
+l3 = input("Enter fav language Tina: \n")
+l4 = input("Enter fav language Bob: \n")
+
+language['sam'] = l1
+language['bill'] = l2
+language['tina'] = l3
+language['bob'] = l4
+
+print(language)
+
+
+
